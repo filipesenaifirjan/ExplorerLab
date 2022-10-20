7ee304b7a074b939ae751b217169b58ff86e3e34
filipesenaifirjan <filipemattossenai@gmail.com>
2022-10-20 09:03:00 -0300

    2

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
 import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+
+ccBgColor01.setAttribute("fill", "green")
+ccBgColor02.setAttribute("fill", "blue")
