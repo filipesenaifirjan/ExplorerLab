91d31ff593242e102a1628b5de0b2ee1eae5e533
filipesenaifirjan <filipemattossenai@gmail.com>
2022-10-21 11:51:57 -0300

    finalizando aula 2

--- src/main.js
+++ src/main.js
@@ -33,10 +33,7 @@ const expirationDatePattern = {
       from: String(new Date().getFullYear()).slice(2),
       to: String(new Date().getFullYear() + 10).slice(2)
     },
-
-
-    MM: {
-
+     MM: {
       mask: IMask.MaskedRange,
       from: 1,
       to: 12,
@@ -44,3 +41,31 @@ const expirationDatePattern = {
   },
 }
 const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+const cardNumber = document.querySelector("#card-number")
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex:/^4\d{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "defaut",
+    },
+  ],
+  dispatch: function (appended, dynamicMasked) {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+    const foundMask = dynamicMasked.compiledMasks.find(function(item) {
+       return number.match(item.regex)
+    })
+    
+    return foundMask
+    },
+ }
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
