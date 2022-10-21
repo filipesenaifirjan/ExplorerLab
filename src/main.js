44d7a3e28c0aff4d6e167a1d2a33be37a4cc3847
filipesenaifirjan <filipemattossenai@gmail.com>
2022-10-21 09:39:28 -0300

    Imask2

--- src/main.js
+++ src/main.js
@@ -26,9 +26,17 @@ const securityCodeMasked = IMask(securityCode, securityCodePattern)
 
 const expirationDate = document.querySelector("#expiration-date")
 const expirationDatePattern = {
-  mask: "MM{/}00",
+  mask: "MM{/}YY",
   blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2)
+    },
+
+
     MM: {
+
       mask: IMask.MaskedRange,
       from: 1,
       to: 12,
